5d99a657536ee688113f8f1454a7db42e2645aaa
Akshaymonkv <70555498+Akshaymonkv@users.noreply.github.com>
2023-12-23 21:02:18 +0530

    Updated the index.py with the db connection

--- pages/index.py
+++ pages/index.py
@@ -1,4 +1,19 @@
 import streamlit as st
+import pymongo
+import pandas as pd
+from pymongo.server_api import ServerApi
+from pymongo.mongo_client import MongoClient
+
+
+@st.cache_resource
+def init_connection():
+    pwd=st.secrets["db_password"]
+    username=st.secrets["db_username"]
+    host=st.secrets["db_host"]
+    uri = f"mongodb+srv://{username}:{pwd}@{host}/?retryWrites=true&w=majority"
+    return pymongo.MongoClient(uri, server_api=ServerApi('1'))
+client = init_connection()
+collection_name = "icd10_codes"
 
 def main():
     st.title('⚕️ S.E.E.R: System for Efficient Encoding and Reference')
@@ -18,11 +33,18 @@ def expand_tile_a():
     st.write("You can use the option to search for ICD Codes and identify the diseases")
 
 
-    search_query = st.text_input("")
+    search_query = st.text_input("Enter your search query")
 
     # Add functionality here
     if st.button("Search"):
         perform_search_tile_a(search_query)
+        results =perform_search_tile_a(search_query)
+        # Convert results to a DataFrame for better display
+        df_results = pd.DataFrame(list(results))
+        st.subheader("Search Results:")
+        st.table(df_results[["ICD10_Code", "Description"]])
+        # for result in results:
+
 
 def expand_tile_b():
  
@@ -34,15 +56,30 @@ def expand_tile_b():
 
     # Add functionality here
     if st.button("Search"):
-        perform_search_tile_b(search_query)
+       perform_search_tile_a(search_query)
+       results =perform_search_tile_a(search_query)
+       # Convert results to a DataFrame for better display
+       df_results = pd.DataFrame(list(results))
+       st.subheader("Search Results:")
+       st.table(df_results[["ICD10_Code", "Description"]])
 
-def perform_search_tile_a(query):
+def perform_search_tile_a(search_query):
 
-    st.write(f"Searching for '{query}'")
+    db = client.icd10_data
+    collection = db['icd10_codes']
+    query = {"Description": {"$regex": search_query, "$options": "i"}}
+    # Perform the search
+    results = collection.find(query)
+    return results
 
-def perform_search_tile_b(query):
+def perform_search_tile_b(search_query):
 
-    st.write(f"Searching for '{query}'")
+    db = client.icd10_data
+    collection = db['icd10_codes']
+    query = {"Description": {"$regex": search_query, "$options": "i"}}
+    # Perform the search
+    results = collection.find(query)
+    return results
 
 if __name__ == "__main__":
     main()
