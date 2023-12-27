795bf24f5b019572f29df405cd414d0463cbb9d1
Akshaymon K V <kvakshaymon@gmail.com>
2023-12-27 21:20:50 +0530

    Fixed the search with download buttons

--- pages/search.py
+++ pages/search.py
@@ -26,54 +26,66 @@ collection_name = "icd10_codes"
 
 def main():
     st.title('⚕️ S.E.E.R: System for Efficient Encoding and Reference')
-    on = st.toggle('Switch to ICD Code Search Mode') # Added toggle support instead of buttons.
+    on = st.toggle('Switch to ICD Code Search Mode ➡️') # Added toggle support instead of buttons.
     if on:
         icd10CodeSearchMode()
     else:
         symptomSearchMode()
 
+
 def icd10CodeSearchMode():
-    st.header("ICD Code Search")
+    st.header("ICD Code Search 📑")
     st.write("You can use the option to search for ICD Codes and identify the diseases")
 
     session_state = st.session_state
     if not hasattr(session_state, 'icd_search_query'):
         session_state.icd_search_query = ""
 
-    icd_search_query = st.text_input("Enter your search query", value=session_state.icd_search_query)
+    icd_search_query = st.text_input("Enter your search query 🔎", value=session_state.icd_search_query)
 
+    search_col, clear_col = st.columns(2)
 
-      
-    if st.button("Search ICD10 code", key="icd10_search_button") and icd_search_query:  # Unique key for tile A
+        
+    if search_col.button("Search ICD10 code ", key="icd10_search_button") and icd_search_query:  # Unique key for tile A
         results = icd10_search(icd_search_query)
         # Convert results to a DataFrame for better display
         df_results = pd.DataFrame(list(results))
         if not df_results.empty:
             st.subheader("Search Results:")
             st.table(df_results[["ICD10_Code", "Description"]])
+
+            csv_data = df_results.to_csv(index=False).encode('utf-8')
+            st.download_button(
+                label="Download Results as CSV 💾",
+                data=csv_data,
+                file_name="icd_search_results.csv",
+                key="download_button"
+            )
+
         else:
            st.warning("No results were found!") 
 
-    if st.button("Clear", key="icd10_clear_button"):
-        session_state.icd_search_query = ""
+        if clear_col.button("Regenerate🔄", key="icd10_clear_button"):
+         session_state.icd_search_query = ""
+         icd_search_query = " "
 
 
 
 
 def symptomSearchMode():
-    st.header("Symptoms search")
+    st.header("Symptoms search 🦠")
     st.write("You can use the option to search for symptoms and identify the ICD Codes for it")
 
     session_state = st.session_state
     if not hasattr(session_state, 'icd_search_query'):
         session_state.icd_search_query = ""
 
-    search_query = st.text_input("Enter your search query",  value=session_state.icd_search_query)  # Provide a label here
+    search_query = st.text_input("Enter your search query 🔎",  value=session_state.icd_search_query)  # Provide a label here
 
-
+    search_col, clear_col = st.columns(2)
 
     # Add functionality here
-    if st.button("Search with Symptoms", key="symptom_button"
+    if search_col.button("Search Symptoms", key="symptom_button"
                  ) and search_query:  # Unique key for tile B
         results = symptomSearch(search_query)
         # Convert results to a DataFrame for better display
@@ -81,11 +93,20 @@ def symptomSearchMode():
         if not df_results.empty:
             st.subheader("Search Results:")
             st.table(df_results[["ICD10_Code", "Description"]])
+
+            csv_data = df_results.to_csv(index=False).encode('utf-8')
+            st.download_button(
+                label="Download Results as CSV 💾",
+                data=csv_data,
+                file_name="symptom_search.csv",
+                key="download_button"
+            )
         else:
             st.warning("No results were found!")
 
-    if st.button("Clear", key="icd10_clear_button"):
-        session_state.icd_search_query = ""
+        if clear_col.button("Regenerate🔄", key="icd10_clear_button"):
+         session_state.icd_search_query = ""
+        # icd_search_query = " "
    
 
 
