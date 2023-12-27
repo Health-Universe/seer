ad4f9fe592747a2c0f87695042b8dbf5ee72cf28
Akshaymon K V <kvakshaymon@gmail.com>
2023-12-27 20:44:29 +0530

    Updated the search functions

--- pages/search.py
+++ pages/search.py
@@ -5,6 +5,7 @@ from pymongo.server_api import ServerApi
 from pymongo.mongo_client import MongoClient
 
 
+
 @st.cache_resource
 def init_connection():
     pwd = st.secrets["db_password"]
@@ -21,6 +22,8 @@ def get_collection(collection_name):
 client = init_connection()
 collection_name = "icd10_codes"
 
+
+
 def main():
     st.title('⚕️ S.E.E.R: System for Efficient Encoding and Reference')
     on = st.toggle('Switch to ICD Code Search Mode') # Added toggle support instead of buttons.
@@ -32,7 +35,17 @@ def main():
 def icd10CodeSearchMode():
     st.header("ICD Code Search")
     st.write("You can use the option to search for ICD Codes and identify the diseases")
-    icd_search_query = st.text_input("Enter your search query")
+
+    session_state = st.session_state
+    if not hasattr(session_state, 'icd_search_query'):
+        session_state.icd_search_query = ""
+
+    icd_search_query = st.text_input("Enter your search query", value=session_state.icd_search_query)
+
+    if st.button("Clear", key="icd10_clear_button"):
+        session_state.icd_search_query = ""
+
+      
     if st.button("Search ICD10 code", key="icd10_search_button") and icd_search_query:  # Unique key for tile A
         results = icd10_search(icd_search_query)
         # Convert results to a DataFrame for better display
@@ -43,14 +56,24 @@ def icd10CodeSearchMode():
         else:
            st.warning("No results were found!") 
 
+
+
 def symptomSearchMode():
-    st.header("Search for Symptoms")
+    st.header("Symptoms search")
     st.write("You can use the option to search for symptoms and identify the ICD Codes for it")
 
-    search_query = st.text_input("Enter your search query")  # Provide a label here
+    session_state = st.session_state
+    if not hasattr(session_state, 'icd_search_query'):
+        session_state.icd_search_query = ""
+
+    search_query = st.text_input("Enter your search query",  value=session_state.icd_search_query)  # Provide a label here
+
+    if st.button("Clear", key="icd10_clear_button"):
+        session_state.icd_search_query = ""
 
     # Add functionality here
-    if st.button("Search with Symptoms", key="symptom_button") and search_query:  # Unique key for tile B
+    if st.button("Search with Symptoms", key="symptom_button"
+                 ) and search_query:  # Unique key for tile B
         results = symptomSearch(search_query)
         # Convert results to a DataFrame for better display
         df_results = pd.DataFrame(list(results))
@@ -59,6 +82,9 @@ def symptomSearchMode():
             st.table(df_results[["ICD10_Code", "Description"]])
         else:
             st.warning("No results were found!")
+
+   
+
 
 def icd10_search(icd_search_query):
     collection = get_collection(collection_name)
